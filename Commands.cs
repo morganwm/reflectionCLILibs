7ec35128c8f49ee84ecb7cd2eeb8d9aab2f45c00
Watson-Morris, Morgan (M) <mwatsonmorris@dow.com>
2017-10-09 12:49:06 -0400

    recompiled

--- Commands.cs
+++ Commands.cs
@@ -4,13 +4,13 @@ using System.IO;
 using System.Collections.Generic;
 
 namespace extra {
-    
+
     public class externalcommandset {
 
         public class externalcmd : ICommand    {
 
             public externalcmd(List<string> input) {
-                input.ForEach(x => Console.WriteLine(x));
+                input.ForEach(x => Console.WriteLine("+" + x));
             }
 
             public bool ExitVal() {
